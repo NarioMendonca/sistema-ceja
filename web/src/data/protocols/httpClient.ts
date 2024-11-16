0132546121dbeb1e9db8b2fc7bc47844002a51d5
Nario Mendonca <nario.sousa2014@gmail.com>
2024-11-16 20:30:13 -0300

    feat: create HttpClient protocol

---
+++ web/src/data/protocols/httpClient.ts
@@ -0,0 +1,27 @@
+export type HttpRequest = {
+  url: string
+  method: HttpMethod
+  body?: any
+  headers?: any
+}
+
+export type HttpResponse<T> = {
+  statusCode: HttpStatusCode
+  body?: T
+}
+
+type HttpMethod = 'get' | 'post' | 'put' | 'delete'
+
+export enum HttpStatusCode {
+  ok = 200,
+  noContent = 204,
+  badRequest = 400,
+  unauthorized = 401,
+  forbidden = 403,
+  notFound = 404,
+  serverError = 500
+}
+
+export interface HttpClient<R = any> {
+  request(data: HttpRequest): Promise<HttpResponse<R>>
+}
