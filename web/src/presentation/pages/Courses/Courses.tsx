4e1ac9de046a1242bd553ab6865d3f7a6739442e
Nario Mendonca <nario.sousa2014@gmail.com>
2024-12-07 23:41:18 -0300

    feat: refactor users and courses page to use page layout

--- web/src/presentation/pages/Courses/Courses.tsx
+++ web/src/presentation/pages/Courses/Courses.tsx
@@ -1,70 +1,65 @@
 import { SearchIcon } from '@/presentation/icons';
 import Styles from './courses-styles.module.scss'
-import { Header, Sidebar } from "@/presentation/components";
+import { PageLayout } from '@/presentation/layouts/PageLayout';
 
 export function Courses() {
   return (
-    <div className={Styles.dashboardContainer}>
-      <Header />
-      <Sidebar />
-
-      <main>
-        <div className={Styles.mainHeaderWrap}>
-          <h2>Gerenciar Matérias</h2>
-          <button>Adicionar matéria</button>
-        </div>
-        <section className={Styles.couseListWrap}>
-          <div className={Styles.searchCourseWrap}>
-            <span>buscar matéria</span>
-            <div className={Styles.searchCourseInputWrap}>
-              <span>{<SearchIcon />}</span>
-              <input type="text" name="searchUser" id="searchUser" />
-            </div>
+    <PageLayout>
+      <div className={Styles.mainHeaderWrap}>
+        <h2>Gerenciar Matérias</h2>
+        <button>Adicionar matéria</button>
+      </div>
+      <section className={Styles.couseListWrap}>
+        <div className={Styles.searchCourseWrap}>
+          <span>buscar matéria</span>
+          <div className={Styles.searchCourseInputWrap}>
+            <span>{<SearchIcon />}</span>
+            <input type="text" name="searchUser" id="searchUser" />
           </div>
-          <table className={Styles.courseList}>
-            <thead>
-              <tr>
-                <th>Nome</th>
-                <th>Professor</th>
-                <th>Alunos</th>
-                <th></th>
-              </tr>
-            </thead>
-            <tbody>
-              <tr>
-                <td>Matemática</td>
-                <td>Caua Lima Andrade</td>
-                <td>25</td>
-                <td>
-                  <div>
-                    <button className={Styles.editButton}>
-                      Editar
-                    </button>
-                    <button className={Styles.deleteButton}>
-                      Deletar
-                    </button>
-                  </div>
-                </td>
-              </tr>
-              <tr>
-                <td>Matemática</td>
-                <td>José Cleudo Paiva Bezerra Junior</td>
-                <td>25</td>
-                <td>
-                  <div>
-                    <button className={Styles.editButton}>
-                      Editar
-                    </button>
-                    <button className={Styles.deleteButton}>
-                      Deletar
-                    </button>
-                  </div>
-                </td>
-              </tr>
-            </tbody>
-          </table>
-        </section>
-      </main>
-    </div>
+        </div>
+        <table className={Styles.courseList}>
+          <thead>
+            <tr>
+              <th>Nome</th>
+              <th>Professor</th>
+              <th>Alunos</th>
+              <th></th>
+            </tr>
+          </thead>
+          <tbody>
+            <tr>
+              <td>Matemática</td>
+              <td>Caua Carvalho</td>
+              <td>25</td>
+              <td>
+                <div>
+                  <button className={Styles.editButton}>
+                    Editar
+                  </button>
+                  <button className={Styles.deleteButton}>
+                    Deletar
+                  </button>
+                </div>
+              </td>
+            </tr>
+            <tr>
+              <td>História</td>
+              <td>Davi Pereira</td>
+              <td>20</td>
+              <td>
+                <div>
+                  <button className={Styles.editButton}>
+                    Editar
+                  </button>
+                  <button className={Styles.deleteButton}>
+                    Deletar
+                  </button>
+                </div>
+              </td>
+            </tr>
+          </tbody>
+        </table>
+      </section>
+    </PageLayout >
   )
 }
--- web/src/presentation/pages/Users/Users.tsx
+++ web/src/presentation/pages/Users/Users.tsx
@@ -1,55 +1,110 @@
 import { SearchIcon } from '@/presentation/icons';
 import Styles from './users-styles.module.scss'
-import { Header, Sidebar } from "@/presentation/components";
+import { PageLayout } from '@/presentation/layouts/PageLayout';
 
 export function Users() {
   return (
-    <div className={Styles.dashboardContainer}>
-      <Header />
-      <Sidebar />
-
-      <main>
-        <div className={Styles.mainHeaderWrap}>
-          <h2>Gerenciar usuários</h2>
-          <button>Adicionar usuário</button>
-        </div>
-        <section className={Styles.userListWrap}>
-          <div className={Styles.searchCourseWrap}>
-            <span>buscar usuário</span>
-            <div className={Styles.searchCourseInputWrap}>
-              <span>{<SearchIcon />}</span>
-              <input type="text" name="searchUser" id="searchUser" />
-            </div>
+    <PageLayout>
+      <div className={Styles.mainHeaderWrap}>
+        <h2>Gerenciar usuários</h2>
+        <button>Adicionar usuário</button>
+      </div>
+      <section className={Styles.userListWrap}>
+        <div className={Styles.searchCourseWrap}>
+          <span>buscar usuário</span>
+          <div className={Styles.searchCourseInputWrap}>
+            <span>{<SearchIcon />}</span>
+            <input type="text" name="searchUser" id="searchUser" />
           </div>
-          <table className={Styles.userList}>
-            <thead>
-              <tr>
-                <th>nome</th>
-                <th>email</th>
-                <th>Permissão</th>
-                <th></th>
-              </tr>
-            </thead>
-            <tbody>
-              <tr>
-                <td>José Cleudo Paiva Bezerra Junior</td>
-                <td>cleudinhoFrifas@gmail.com</td>
-                <td>Admin</td>
-                <td>
-                  <div>
-                    <button className={Styles.editButton}>
-                      Editar
-                    </button>
-                    <button className={Styles.deleteButton}>
-                      Deletar
-                    </button>
-                  </div>
-                </td>
-              </tr>
-            </tbody>
-          </table>
-        </section>
-      </main>
-    </div>
+        </div>
+        <table className={Styles.userList}>
+          <thead>
+            <tr>
+              <th>nome</th>
+              <th>email</th>
+              <th>Permissão</th>
+              <th></th>
+            </tr>
+          </thead>
+          <tbody>
+            <tr>
+              <td>Rogerio Tech</td>
+              <td>cleudinhoFrifas@gmail.com</td>
+              <td>Admin</td>
+              <td>
+                <div>
+                  <button className={Styles.editButton}>
+                    Editar
+                  </button>
+                  <button className={Styles.deleteButton}>
+                    Deletar
+                  </button>
+                </div>
+              </td>
+            </tr>
+            <tr>
+              <td>Armando da Silva</td>
+              <td>cleudinhoFrifas@gmail.com</td>
+              <td>Professor</td>
+              <td>
+                <div>
+                  <button className={Styles.editButton}>
+                    Editar
+                  </button>
+                  <button className={Styles.deleteButton}>
+                    Deletar
+                  </button>
+                </div>
+              </td>
+            </tr>
+            <tr>
+              <td>Pedro Carvalho Alves</td>
+              <td>cleudinhoFrifas@gmail.com</td>
+              <td>Aluno</td>
+              <td>
+                <div>
+                  <button className={Styles.editButton}>
+                    Editar
+                  </button>
+                  <button className={Styles.deleteButton}>
+                    Deletar
+                  </button>
+                </div>
+              </td>
+            </tr>
+            <tr>
+              <td>Roger da paz</td>
+              <td>cleudinhoFrifas@gmail.com</td>
+              <td>Aluno</td>
+              <td>
+                <div>
+                  <button className={Styles.editButton}>
+                    Editar
+                  </button>
+                  <button className={Styles.deleteButton}>
+                    Deletar
+                  </button>
+                </div>
+              </td>
+            </tr>
+            <tr>
+              <td>Gabriel Sovares</td>
+              <td>cleudinhoFrifas@gmail.com</td>
+              <td>Aluno</td>
+              <td>
+                <div>
+                  <button className={Styles.editButton}>
+                    Editar
+                  </button>
+                  <button className={Styles.deleteButton}>
+                    Deletar
+                  </button>
+                </div>
+              </td>
+            </tr>
+          </tbody>
+        </table>
+      </section>
+    </PageLayout>
   )
 }
