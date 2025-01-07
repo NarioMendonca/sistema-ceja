07e77d04c386d756ccfdf9b6bac764271204b336
NarioMendonca <nario.sousa2014@gmail.com>
2025-01-07 17:46:34 -0300

    fix: fix register user

--- server/src/usecases/users/register-user.ts
+++ server/src/usecases/users/register-user.ts
@@ -1,35 +1,37 @@
 import type { UsersRepository } from '@/repositories'
-import { AlreadyExistsError } from '../errors'
+import { AlreadyExistsError, InvalidRoleError } from '../errors'
 import { UserRoles, type UserWithoutPasswordHash } from '@/models/User'
 import {
 	createPasswordHash,
 	generateRandomPassword,
 } from '@/utils/passwordUtils'
 
-interface RegisterUseCaseRequest {
+type UserData = {
 	name: string
 	email: string
-	role?: UserRoles
+	role: UserRoles
 	birth?: Date
 	cpf?: string
+	enrollmentCode?: string
+	dateOfBirth?: Date
+	adress?: string
+	specialization?: string
+	education?: string
+	position?: string
 }
 
-interface RegisterUseCaseResponse {
-	user: UserWithoutPasswordHash
+interface RegisterUseCaseRequest {
+	userData: UserData
 }
 
 export class RegisterUseCase {
 	constructor(private usersRepository: UsersRepository) { }
 
 	async execute({
-		email,
-		name,
-		role,
-		birth,
-		cpf,
-	}: RegisterUseCaseRequest): Promise<RegisterUseCaseResponse> {
+		userData
+	}: RegisterUseCaseRequest): Promise<void> {
 		const verifyIfEmailAlreadyExists =
-			await this.usersRepository.findByEmail(email)
+			await this.usersRepository.findByEmail(userData.email)
 
 		if (verifyIfEmailAlreadyExists) {
 			throw new AlreadyExistsError()
@@ -40,19 +42,38 @@ export class RegisterUseCase {
 
 		// create here a function to send an email with user password and a link to be able to change
 
-		const user = await this.usersRepository.create({
-			name,
-			email,
-			password_hash: passwordHashed,
-			role: role ?? UserRoles.student,
-			birth: birth ?? null,
-			cpf: cpf ?? null,
-		})
-
-		const { password_hash, ...userWithoutPasswordHash } = user
-
-		return {
-			user: userWithoutPasswordHash,
+		if (userData.role === 'ADMIN') {
+			const user = await this.usersRepository.CreateAdmin({
+				name: userData.name,
+				email: userData.email,
+				password_hash: passwordHashed,
+				position: userData.position ?? '',
+				role: UserRoles.admin,
+				cpf: userData.cpf ?? null
+			})
+		} else if (userData.role === 'TEACHER') {
+			const user = await this.usersRepository.CreateTeacher({
+				name: userData.name,
+				email: userData.email,
+				password_hash: passwordHashed,
+				role: UserRoles.teacher,
+				cpf: userData.cpf ?? null,
+				education: userData.education ?? '',
+				specialization: userData.specialization ?? ''
+			})
+		} else if (userData.role === 'STUDENT') {
+			const user = await this.usersRepository.createStudent({
+				name: userData.name,
+				email: userData.email,
+				password_hash: passwordHashed,
+				role: UserRoles.student,
+				cpf: userData.cpf ?? null,
+				adress: userData.adress ?? '',
+				dateOfBirth: userData.dateOfBirth ?? null,
+				enrollmentCode: userData.enrollmentCode ?? ''
+			})
+		} else {
+			throw new InvalidRoleError()
 		}
 	}
 }
