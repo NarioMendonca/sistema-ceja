829cc510d2e712911cbc78708b13f7474e9ae00a
NarioMendonca <nario.sousa2014@gmail.com>
2025-01-09 21:51:12 -0300

    feat: create routes for courses controllers

---
+++ server/src/controllers/courses/courses-routes.ts
@@ -0,0 +1,8 @@
+import { FastifyInstance } from "fastify";
+import { createCourse, fetchCourses, } from "./";
+
+export async function coursesRoutes(app: FastifyInstance) {
+  app.get('/courses', fetchCourses)
+
+  app.post('/courses', createCourse)
+}
---
+++ server/src/controllers/courses/index.ts
@@ -0,0 +1,2 @@
+export * from './create-course'
+export * from './fetch-courses'
