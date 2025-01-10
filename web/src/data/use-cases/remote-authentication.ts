3fd8ccc4d6c39b3bec50408e6881b09870252d71
NarioMendonca <nario.sousa2014@gmail.com>
2025-01-10 15:45:52 -0300

    fix: put withCredentials on remote-authentication to accept refresh token

--- web/src/data/use-cases/remote-authentication.ts
+++ web/src/data/use-cases/remote-authentication.ts
@@ -2,6 +2,7 @@ import type { Authentication } from "@/domain/use-cases/authentication";
 import { HttpClient } from "../protocols/httpClient";
 import { UnexpectedError } from "@/domain/errors";
 import { InvalidCredentialsError } from "@/domain/errors/invalid-credentials-error";
+import axios from "axios";
 
 export class RemoteAuthentication implements Authentication {
   constructor(
@@ -10,10 +11,15 @@ export class RemoteAuthentication implements Authentication {
   ) { }
 
   async auth(params: Authentication.Params): Promise<Authentication.Model> {
-    const httpResponse = await this.httpClient.request({ url: this.url, method: 'post', body: params })
+    const httpResponse = await axios({
+      baseURL: this.url,
+      method: 'post',
+      data: params,
+      withCredentials: true
+    })
 
-    switch (httpResponse.statusCode) {
-      case 200: return httpResponse.body!
+    switch (httpResponse.status) {
+      case 200: return httpResponse.data!
       case 401: throw new InvalidCredentialsError()
       default: throw new UnexpectedError()
     }
