cc75a32db22223427b391964f550e9472b1cc6af
NarioMendonca <nario.sousa2014@gmail.com>
2025-01-25 19:27:41 -0300

    feat: create get users metrics controller

--- server/src/controllers/users/users-routes.ts
+++ server/src/controllers/users/users-routes.ts
@@ -4,8 +4,10 @@ import { getUser } from "./get-user";
 import { registerUser } from "./register-user";
 import { authenticateUser } from "./authenticate-user";
 import { refresh } from "./refresh";
+import { getUsersMetrics } from "./get-users-metrics";
 
 export function usersRoutes(app: FastifyInstance) {
+  app.get('/users/metrics', getUsersMetrics)
   app.get('/user/:userId', getUser)
   app.get('/users', fetchUsers)
 
