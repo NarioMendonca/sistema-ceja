25f749fc6fe6311b63c8e33ad5b6ae19afc46116
Nario Mendonca <122683077+NarioMendonca@users.noreply.github.com>
2025-03-05 18:57:58 -0300

    Feat/create grades page (#14)
    * feat: create accordion
    
    * feat: create grades and use with accordion

---
+++ web/src/presentation/components/Accordion/index.tsx
@@ -0,0 +1,9 @@
+import { AccordionContent } from "./AccordionContent";
+import { AccordionContextProvider } from "./AccordionContext";
+import { AccordionHeader } from "./AccordionHeader";
+
+export const Accordion = {
+  Provider: AccordionContextProvider,
+  Header: AccordionHeader,
+  Content: AccordionContent
+}
--- web/src/presentation/pages/GradesView/GradesView.tsx
+++ web/src/presentation/pages/GradesView/GradesView.tsx
@@ -1,16 +1,33 @@
 import Styles from './grades-view-styles.module.scss'
-import { MainHeader } from "./components/MainHeader";
-import { GradeAccordion } from "./components/GradeAccordion";
+import { Grade } from "./components/Grade";
+import { MainHeader } from './components/MainHeader';
+import { Accordion } from '@/presentation/components/Accordion';
 
 export function GradesView() {
   return (
-    <>
+    <main className={Styles.gradesViewContainer}>
       <MainHeader />
-      <section className={Styles.viewGradesWrap}>
-        <GradeAccordion />
-        <GradeAccordion />
-        <GradeAccordion />
-      </section>
-    </>
+      <Accordion.Provider>
+        <Accordion.Header>
+          <Grade
+            title='Matemática'
+            value={7.5}
+            description='Média aritmética'
+          />
+        </Accordion.Header>
+        <Accordion.Content extraClassName={Styles.contentGradeWrap}>
+          <Grade
+            title='Prova'
+            value={6}
+            description='Nota refente a prova'
+          />
+          <Grade
+            title='Trabalho'
+            value={8}
+            description=''
+          />
+        </Accordion.Content>
+      </Accordion.Provider>
+    </main>
   )
 }
